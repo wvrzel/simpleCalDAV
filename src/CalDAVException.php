19aa30aa4d1fa1e2a64136005c537a30aa343ac8
frankminkenberg <36509636+frankminkenberg@users.noreply.github.com>
2020-03-27 08:29:25 +0100

    Wrong namespace used for DOMDocument

--- src/CalDAVException.php
+++ src/CalDAVException.php
@@ -31,7 +31,7 @@ class CalDAVException extends \Exception {
     
     public function __toString() {
     	$string = '';
-    	$dom = new DOMDocument();
+    	$dom = new \DOMDocument();
     	$dom->preserveWhiteSpace = FALSE;
 		$dom->formatOutput = TRUE;
 		
